1f3d51ce80a0466427acf17f521f9b0aa2b43d8d
iiitoo <zy@iiitoo.com>
2018-07-10 09:08:18 +0800

    变量

---
+++ books.js
@@ -0,0 +1,11 @@
+var book1Title = "The Hobbit";
+var book1Author = "J.R.R.Tolkien";
+var book2Title = "Northern Lights";
+var book2Author = "Philip Pullman";
+var book3Title = "The Adventtures of Tom Sawyer";
+var book3Author = "Mark Twain";
+console.log("There are three books so far……");
+console.log(book1Title + " by " + book1Author);
+console.log(book2Title + " by " + book2Author);
+console.log(book3Title + " by " + book3Author);
+
